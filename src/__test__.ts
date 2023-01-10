86742f0125e8991ac6c7012e29ca48fdb6780a7f
itsjusttriz <itsjusttriz@gmail.com>
2023-01-10 02:08:32 +0000

    Updated compilerOptions & testing file

---
+++ src/__test__.ts
@@ -0,0 +1,32 @@
+import { IArray, IObject, IRequest, IString, JSONResponse, wait } from '.';
+
+async function runExamples() {
+    const arr = [1, 2, 3, 4, 5, 6];
+    const WAIT_TIME = 3 * 1000;
+
+    wait(WAIT_TIME).then(() => console.log("Finished Waiting."))
+
+
+    console.log({
+
+        array_random: new IArray(arr).getRandom(),
+        array_first: new IArray(arr).getFirst(),
+        array_last: new IArray(arr).getLast(),
+
+        objStringifyKeyVal: IObject.stringifyKeyValue({ name: 'IJT', age: 23, isOver18: true }),
+        objMap: IObject.map({ age: 10 }, ([key, val]) => [key, val*2]),
+
+        reqGetMethod: IRequest.getMethod('POST'),
+
+        stringIsNumeric: IString.isNumeric('10'),
+
+        json_resp: new JSONResponse().setCode(
+            IRequest.getStatusCode('OK')
+        ).setMessage(
+            'This is a test.'
+        ).setPayload({
+                success: true
+        })
+    })
+};
+runExamples();
